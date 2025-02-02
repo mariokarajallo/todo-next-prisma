51bcc09dbc286e84a080ae3f862b9b921c24e01c
Mario Karajallo <mariokarajallo92@gmail.com>
2025-02-02 17:54:24 -0300

    Agregar autenticación con NextAuth y configurar el proveedor de GitHub; incluir el componente AuthProvider y actualizar la barra lateral para mostrar información del usuario conectado.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,26 @@
 import { WidgetItem } from "@/components";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    console.log("No session");
+    redirect("/api/auth/signin");
+  }
+  console.log(session.user);
+
   return (
-    <div>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {/* <WidgetItem /> */}
-      </div>
+    <div className="grid gap-6 grid-cols-1 md:grid-cols-2">
+      <WidgetItem title="Usuario Conectado Server Side">
+        <div className="flex flex-col">
+          <span>{session.user?.name}</span>
+          <span>{session.user?.image}</span>
+          <span>{session.user?.email}</span>
+        </div>
+      </WidgetItem>
     </div>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,10 @@ import {
   CiViewList,
 } from "react-icons/ci";
 import { SidebarItem } from "@/components";
-import { IoCode } from "react-icons/io5";
+import { IoCode, IoPerson, IoPersonOutline } from "react-icons/io5";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import path from "path";
 
 const menuItems = [
   {
@@ -35,9 +38,22 @@ const menuItems = [
     title: "Products",
     path: "/dashboard/products",
   },
+  {
+    icon: <IoPersonOutline />,
+    title: "Profile",
+    path: "/dashboard/profile",
+  },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+
+  const avatarUrl = session?.user?.image
+    ? session.user.image
+    : "https://avatar.iran.liara.run/public/29";
+
+  const userName = session?.user?.name ?? "No Name";
+
   return (
     <div>
       <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
@@ -58,14 +74,14 @@ export const Sidebar = () => {
           <div className="mt-8 text-center">
             {/* Next/Image */}
             <Image
-              src="https://avatar.iran.liara.run/public/29"
+              src={avatarUrl}
               alt=""
               className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
               width={40}
               height={40}
             />
             <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-              Cynthia J. Watts
+              {userName}
             </h5>
             <span className="hidden text-gray-400 lg:block">Admin</span>
           </div>
